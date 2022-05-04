fbdcdc2d91e455fb443e2fe18e8c0829dbd2eeba
Artem Dvirnyak <artem.dvirnyak@gmail.com>
2022-05-05 02:28:05 +0300

    new commands

--- bot.py
+++ bot.py
@@ -14,6 +14,24 @@ updater = Updater(token=telegram_bot_token, use_context=True)
 dispatcher = updater.dispatcher
 lock = Lock()
 
+def load_users():
+    with open("users.json", "r") as f:
+        users = json.load(f)
+    return users
+
+def load_rates():
+    with open("rates.json", "r") as f:
+       rates = list(map(float, json.load(f)))
+    return rates
+
+def update_users(users):
+    with open("users.json", "w") as f:
+        json.dump(users, f)
+
+def update_rates(rates):
+    with open("rates.json", "w") as f:
+        json.dump(rates, f)
+
 def start(update, context):
     chat_id = update.effective_chat.id
     context.bot.send_message(chat_id=chat_id,
@@ -21,15 +39,22 @@ def start(update, context):
                                   "а также предупреждать о резких скачках(можно настроить). Давай начнём")
     lock.acquire()
 
-    with open("users.json", "r") as f:
-        users = json.load(f)
-
+    users = load_users()
     users[chat_id] = {"state": "default", "limits": []}
-
-    with open("users.json", "w") as f:
-        json.dump(users, f)
+    update_users(users)
 
     lock.release()
+
+
+def help(update, context):
+    chat_id = update.effective_chat.id
+
+    text = 'Доступные команды: \n\n/rate - текущий курс' \
+           '\n/new - добавить новое отслеживание\n' \
+           '/trackings - посмотреть текущие отслеживания' \
+           '\n/delete N - удалить отслеживание номер N'
+    context.bot.send_message(chat_id=chat_id,
+                             text=text)
 
 
 def rates_msg(rate_new, rate_old, time1):
@@ -45,15 +70,45 @@ def rates_msg(rate_new, rate_old, time1):
     return msg
 
 
+def trackings(update, context):
+    chat_id = str(update.effective_chat.id)
+
+    users = load_users()
+
+    index = 0
+    msg = 'Твои отслеживания:\n\n'
+    for limit in users[chat_id]['limits']:
+        index += 1
+        msg += "{index}. На {percent}% за {sec} секунд\n".format(
+            index=index, percent=limit['diff'], sec=limit['time'])
+
+    context.bot.send_message(chat_id=chat_id, text=msg)
+
+
+def delete(update, context):
+    chat_id = str(update.effective_chat.id)
+
+    lock.acquire()
+    users = load_users()
+
+    user_msg = update.message.text
+    index = int(user_msg[8:]) - 1
+    del users[chat_id]['limits'][index]
+    msg = "Как скажешь"
+
+    update_users(users)
+    lock.release()
+
+    context.bot.send_message(chat_id=chat_id, text=msg)
+    trackings(update, context)
+
 def new_tracking(update, context):
     lock.acquire()
-    users = {}
 
-    with open("users.json", "r") as f:
-        users = json.load(f)
-
+    users = load_users()
     chat_id = str(update.effective_chat.id)
     user_msg = update.message.text
+    msg = ''
 
     if (users[chat_id]['state'] == "default" and user_msg == "/new"):
         users[chat_id]['state'] = "enter diff"
@@ -71,16 +126,12 @@ def new_tracking(update, context):
         msg = "Запомнил. Слежу внимательнее"
 
     context.bot.send_message(chat_id=chat_id, text=msg)
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
+    update_users(users)
     lock.release()
 
 
 def rate(update, context):
-    lock.acquire()
-    with open("rates.json", "r") as f:
-        rates_history = list(map(float, json.load(f)))
-    lock.release()
+    rates_history = load_rates()
 
     chat_id = update.effective_chat.id
     msg = "1 DOGE = {} USDT\n\n".format(rates_history[-1])
@@ -97,11 +148,8 @@ def notifier():
     delay = 1
     while True:
         lock.acquire()
-        with open("users.json", "r") as f:
-            users = json.load(f)
-
-        with open("rates.json", "r") as f:
-            rates_history = list(map(float, json.load(f)))
+        users = load_users()
+        rates_history = load_rates()
 
         for chat_id in users.keys():
             user = users[chat_id]
@@ -114,26 +162,25 @@ def notifier():
                 if old_rate == 0 or new_rate == 0:
                     continue
 
-                if (new_rate > old_rate and (new_rate / old_rate - 1) >= diff) \
-                        or (new_rate < old_rate and (1 - new_rate / old_rate) >= diff)\
-                        and limit['last_check'] - time.time() > time_diff:
+                if ((new_rate > old_rate and (new_rate / old_rate - 1) >= diff) \
+                        or (new_rate < old_rate and (1 - new_rate / old_rate) >= diff))\
+                        and (int(time.time()) - int(limit['last_check']) > time_diff):
                     msg += rates_msg(new_rate, old_rate, time_diff-1) + '\n'
+
                     users[chat_id]['limits'][index]['last_check'] = int(time.time())
 
             if len(msg) > 0:
                 msg = "1 DOGE = {} USDT\n\n".format(rates_history[-1]) + msg
                 updater.bot.send_message(chat_id=chat_id,
                                          text=msg)
-        with open('users.json', 'w') as f:
-            json.dump(users, f)
+        update_users(users)
         lock.release()
         time.sleep(delay)
 
 
 def checker():
     delay = 1
-    with open("rates.json", "r") as f:
-        rates_history = list(map(float, json.load(f)))
+    rates_history = load_rates()
 
     key = "https://api.binance.com/api/v3/ticker/price?symbol="
     currencies = ["DOGEUSDT"]
@@ -147,8 +194,7 @@ def checker():
             rates_history.append(data['price'])
 
         lock.acquire()
-        with open('rates.json', 'w') as f:
-            json.dump(rates_history, f)
+        update_rates(rates_history)
         lock.release()
 
         time.sleep(delay)
@@ -161,6 +207,9 @@ def bot():
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("rate", rate))
 dispatcher.add_handler(CommandHandler("new", new_tracking))
+dispatcher.add_handler(CommandHandler("help", help))
+dispatcher.add_handler(CommandHandler("trackings", trackings))
+dispatcher.add_handler(CommandHandler("delete", delete))
 dispatcher.add_handler(MessageHandler(Filters.text, new_tracking))
 
 Process(target=checker).start()
